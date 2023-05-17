ece0279d860e7a33347fd525a608da2a4fe18489
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-05-17 15:03:50 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,10 @@
-const App = () => <div>App</div>;
+import { useState, useEffect } from "react";
+const App = () => {
+  const [intro, setIntro] = useState(true);
+  useEffect(() => {
+    const t = setTimeout(() => setIntro(false), 3000);
+    return () => clearTimeout(t);
+  }, []);
+  return intro ? <div>App</div> : <></>;
+};
 export default App;
