7c90c6bd992d321bc08d4c118e87484ad35d84fc
Michael Nusair <mnpcmw6444@gmail.com>
2023-10-16 14:18:38 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Timeline from './Timeline';
 
+
 function App() {
     const projects = [
-        {name: "IAF TrackIshi", startDate: "10/01/2021", endDate: "13/06/2022"},
-        {name: "IAF Ofek324", startDate: "13/06/2022", endDate: "01/01/2024"},
+        {name: "IAF TrackIshi", startDate: "10/01/2021", endDate: "01/01/2023"},
+        {name: "IAF Ofek324", startDate: "13/06/2022", endDate: "today"},
         {name: "Neurobica Supplements", startDate: "01/03/2021", endDate: "01/04/2023"},
-        {name: "Neurobicare", startDate: "15/12/2022", endDate: "01/07/2023"},
-        {name: "CapHub.ai", startDate: "01/03/2023", endDate: "01/07/2023"},
-        {name: "Neurobica.io", startDate: "15/12/2022", endDate: "01/07/2023"},
-        {name: "Failean", startDate: "01/07/2023", endDate: "01/01/2024"},
-        {name: "Offisito", startDate: "01/09/2023", endDate: "01/01/2024"},
-        {name: "DiveOps", startDate: "01/09/2023", endDate: "01/01/2024"},
+        {name: "Neurobicare", startDate: "01/01/2022", endDate: "today"},
+        {name: "CapHub.ai", startDate: "01/03/2023", endDate: "01/08/2023"},
+        {name: "Neurobica.io", startDate: "01/08/2023", endDate: "today"},
+        {name: "Failean", startDate: "01/07/2023", endDate: "today"},
+        {name: "Offisito", startDate: "01/09/2023", endDate: "today"},
+        {name: "DiveOps", startDate: "01/09/2023", endDate: "today"},
     ];
 
     return (
